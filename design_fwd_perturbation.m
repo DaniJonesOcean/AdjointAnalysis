3a0fb5a8477144da9799c8cdcd73aced1d711db0
Dan Jones <dcjones.work@gmail.com>
2017-06-13 14:11:14 +0100

    adxx/ADJ generalisation

---
+++ design_fwd_perturbation.m
@@ -0,0 +1,12 @@
+%
+% design forward perturbation experiment using adxx files
+%
+
+%% initial setup
+initial_setup;
+
+%% what should the xx_* file look like?
+
+%% also need to select timescale, sigma scale
+
+%% load relevant adxx files
---
+++ get_mixlayerdepth.m
@@ -0,0 +1,25 @@
+%
+% get mixed layer depth for this record
+%
+
+% To use this, the time periods of the sea ice diagnostic output 
+% must match the output periods of the adxx/ADJ files. There is *nothing* in 
+% this script to make sure that condition is true, so it's up to you. 
+%
+
+% load sea ice concentration
+if nrecord<=length(its_ad)
+
+  % name for rdmds2gcmfaces call
+  mld_location = strcat(rootdir,'experiments/',fwddir,'diag_2D_set1');
+
+  % load sea ice area
+  mld_now = rdmds2gcmfaces(mld_location,its_ad(nrecord),'rec',1);
+
+else
+
+  warning('---------- get_mixlayerdepth.m :: nrecord>length(its_ad)')
+  warning('---------- mixed layer not loaded here')
+  mld_now = [];
+
+end     
