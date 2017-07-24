f613449ced8ffa99f79e1b24cb14ea478b7ff885
Emma Boland <emmomp@bas.ac.uk>
2017-07-24 14:20:10 +0100

    Fixed small error in generic_stats Added make_T_mask, a function that creates a time mask, assuming 1 month averaging period Changed make_MLD_mask into a function that creates a 3D spatial mask based on ECCO mixed layer stats

--- make_mld_mask.m
+++ make_mld_mask.m
@@ -1,6 +1,14 @@
+function make_mld_mask(years,months,masktype,maskname,areamask)
 %makes 3D masks based on ECCO MLD depths. Specify time range and mean or
 %max and name for output file
+%years = {2007,2008} Years to MLD from
+%months = {12,[1,2]} Months corresponding to those years
+%masktype = 'mean' choose mean or max MLD
+%maskname = 'south30_mld95_mean' Name for mask
+%areamask = 'south30S' OPTIONAL choose area mask that exists in masks directory
 
+addpath /users/dannes/gcmfaces/
+addpath /users/dannes/matlabfiles/m_map/
 inputs
 fwdloc = strcat(fwdroot,'experiments/',fwddir);
 % if 'its.txt' file exists (list of iteration numbers), load it
@@ -11,15 +19,16 @@ else
     disp('-- initial_setup: no its.txt file detected')
     disp('--')
 end
-
-%years = {2007,2008};
-%months = {12,[1,2]};
-years = {1995};
-months = {[1:12]};
-areamask = 'south30S'; %choose area mask that exists in masks directory
-masktype = 'mean'; % choose mean or max MLD
-maskname = 'south30_mld95_mean';
-gcmfaces_global;
+gcmfaces_global;    
+gloc = strcat(rootdir,'grid/');
+    if exist(gloc,'dir')
+      disp('--')
+      disp(strcat('-- grid location: ',gloc))
+      disp('--')
+    else 
+      error('-- Grid files not found, check gloc in initial_setup.m')
+    end
+grid_load(gloc,5,'compact',1);
 
 % Calculate fwd run dates
 dv = nan(length(its),6);
@@ -41,8 +50,13 @@ its_out = its(logical(idates));
 %Load mixed layer
 mld = get_mixlayerdepth(its_out',fwdroot,fwddir);
 
-%Take time mean
-mld_tmean = squeeze(nanmean(mld,3));
+%Take time mean/max
+switch masktype
+    case 'mean'
+        mld_tmean = squeeze(nanmean(mld,3));
+    case 'max'
+        mld_tmean = squeeze(max(mld,[],3));        
+end
 
 mld_SO = mld_tmean(mygrid.YC<=-30);
 
@@ -55,10 +69,13 @@ display('Mean :')
 nanmean(mld_SO)
 
 %Load area mask
-if exist(['masks/' areamask], 'file')
-    areafld = read_bin(['masks/' areamask]);
-else
-    display('-- Area mask not found --')
+if nargin > 4
+    display('Area mask defined')
+    if exist(['masks/' areamask], 'file')
+        areafld = read_bin(['masks/' areamask]);
+    else
+        display('-- Area mask not found --')
+    end
 end
 
 %Make depth mask
@@ -67,7 +84,11 @@ maskfld = mld_tmean;
 for iFace=1:mld_tmean.nFaces
         iF=num2str(iFace);
         eval(['fac = mld_tmean.f' iF ';'])
-        eval(['amask = areafld.f' iF ';'])       
+        if nargin > 4
+            eval(['amask = areafld.f' iF ';'])       
+        else
+            amask = ones(size(fac));
+        end
         mask = zeros([size(fac),length(depths)]);
         for x = 1:size(fac,1)
             for y = 1:size(fac,2)
@@ -81,5 +102,7 @@ for iFace=1:mld_tmean.nFaces
         eval(['maskfld.f' iF '=mask;'])
 end
 
-write2file(['masks/' maskname '_mask'],convert2gcmfaces(maskfld))
-figure, m_map_gcmfaces(sum(maskfld,3))
+write2file(['masks/' maskname '_maskC'],convert2gcmfaces(maskfld))
+display(['Mask written to masks/' maskname '_maskC'])
+figure, m_map_gcmfaces(sum(maskfld,3),3)
+title([maskname ' level'])
